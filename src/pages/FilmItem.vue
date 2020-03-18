0f1a0717805c49dbe686285746a46d9dbe29b7e1
thiery margot <margot.thiery@gmail.com>
2020-03-18 12:34:49 +0100

    page film item in progress

--- src/pages/FilmItem.vue
+++ src/pages/FilmItem.vue
@@ -1,33 +1,14 @@
 <template>
-  <section class="section body-section">
-      <div>{{$route.params.id}}</div>
-        <!-- <div class="columns is-multiline">
-            <div class="column is-full" v-for="film in films" :key="film.id">
-                <div class="box">
-                <article class="media">
-                    <div class="media-left">
-                        <figure class="image is-3by4" style="width: 100px">
-                            <img src="../assets/fffff.png">
-                        </figure>
-                    </div>
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                                <strong>{{film.title}}</strong> 
-                            <br>
-                                {{film.date}}                                 
-                            <br>
-                            <br>
-                                <strong>Synopsis</strong>
-                            <br>
-                                {{film.synopsis}}
-                            </p>
-                        </div>
-                    </div>
-                </article>
-                </div>
-            </div>
-        </div> -->
+    <section class="section body-section">
+        <img src="../assets/fffff.png" class="affiche">
+        <strong class="title is-1">{{film.title}}</strong> 
+        <br>
+        {{film.date}}                                 
+        <br>
+        <br>
+        <strong>Synopsis</strong>
+        <br>
+        {{film.synopsis}}
     </section>
 </template>
 
@@ -37,7 +18,19 @@ export default {
             
         },
         computed: {
-            
+            film() {
+                // console.log(this.$route)
+                let findFilm = this.$store.state.films.filter(film =>
+                    film.id == this.$route.params.id)[0]
+                return findFilm
+            }
         }
     };
 </script>
+<style scoped>
+    .affiche {
+        width: 210px;
+        float: left;
+        margin: 0px 20px 10px 0px;
+    }
+</style>
